48cf719a86ba6d5e3cb95bf840df31e9342e4d18
Pranav K <pranavkcse@gmail.com>
2024-08-10 15:59:17 +0530

    project completed with openweather api integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-const App = () => {
+import Forecast from './components/Forecast'
+import Search from './components/Search'
+import useForecast from './hooks/useForecast'
+
+const App = (): JSX.Element => {
+  const { term, options, forecast, onSubmit, onInputChange, onOptionSelect } =
+    useForecast()
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      {forecast ? (
+        <Forecast data={forecast} />
+      ) : (
+        <Search
+          term={term}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+          options={options}
+        />
+      )}
     </main>
   )
 }
---
+++ src/hooks/useForecast.ts
@@ -0,0 +1,63 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+import { forecastType, optionType } from '../types'
+
+const useForecast = () => {
+  const [term, setTerm] = useState<string>('')
+  const [city, setCity] = useState<optionType | null>(null)
+  const [options, setOptions] = useState<[]>([])
+  const [forecast, setForecast] = useState<forecastType | null>(null)
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data))
+  }
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    setTerm(value)
+    getSearchOptions(value)
+  }
+  const getForcast = (city: optionType) => {
+    const { lat, lon } = city
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const forecastData = {
+          ...data.city,
+          list: data.list.slice(0, 16),
+        }
+        setForecast(forecastData)
+      })
+  }
+  const onSubmit = () => {
+    if (!city) return
+    getForcast(city)
+  }
+
+  const onOptionSelect = (option: optionType) => {
+    setCity(option)
+  }
+
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name)
+      setOptions([])
+    }
+  }, [city])
+
+  return {
+    term,
+    options,
+    forecast,
+    onSubmit,
+    onInputChange,
+    onOptionSelect,
+  }
+}
+
+export default useForecast
